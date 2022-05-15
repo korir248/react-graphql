b4421594edef854a3b259b3cf556ef5478af3194
Eugene Korir <korir16071@gmail.com>
2022-05-15 23:13:40 +0300

    modified data.jsx

--- src/Components/Data.jsx
+++ src/Components/Data.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect} from 'react'
 import axios from 'axios'
 import { useQuery} from 'react-query'
+import { useState } from 'react'
 
 
 const endpoint = 'https://fullstack-engineer-test-n4ouilzfna-uc.a.run.app/graphql'
@@ -14,32 +15,47 @@ const pages_query  = `{
 
 
 const Data= () => {
+  const [data, setData] = useState({})
 
   useEffect(() => {
-  //Some axios code goes here  
+  //Some axios code goes here
   
-    
+  const fetchData = ()=>{
+    const {data} = axios.post(endpoint,{query: pages_query})
+    .then(response => {
+      console.log(response)
+      return response
+    })
+      
+    .catch(err => console.log(err.message)) 
+    setData(data)
+  }
+  fetchData()
+
+
   }, [])
   
-  const { data, isLoading,error } = useQuery("pages", async () => {
+  // const { data, isLoading,error } = useQuery("pages", async () => {
 
-    try {
-      const {data} = await axios.post(endpoint,{query: pages_query})      
+  //   try {
+  //     const {data} = await axios.post(endpoint,{query: pages_query}) 
       
-      return data.data
+  //     console.log(data)
+      
+  //     return data.data
   
-    } catch (error) {
-      console.log(error.message);
+  //   } catch (error) {
+  //     console.log(error.message);
       
-    }
-  }).error(error=> console.log(error))
+  //   }
+  // })
 
-  if (isLoading) return "Loading...";
-  if (error) return <pre>{error.message}</pre>;
+  // if (isLoading) return "Loading...";
+  // if (error) return <pre>{error.message}</pre>;
 
   return (
     <div>
-      data: {data}
+      {/* data: {data.data} */}
 
     </div>
   )
