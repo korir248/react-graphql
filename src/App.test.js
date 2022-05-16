75eff8073a17b2f5d1e64aa1c66118759aa2f2af
Eugene Korir <korir16071@gmail.com>
2022-05-16 22:00:31 +0300

    added a test in App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Data from './Components/Data';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Left/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+
+test('should render Data component', () => { 
+  render(<Data/>)
+ })
